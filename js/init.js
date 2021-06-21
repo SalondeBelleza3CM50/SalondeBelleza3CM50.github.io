41c089f5631233b704ea76d0d486b186d66d0348
SalondeBelleza3CM50 <86210129+SalondeBelleza3CM50@users.noreply.github.com>
2021-06-20 23:36:38 -0500

    Delete js directory

--- js/init.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp ({
-    apiKey: "AIzaSyA-wI0VNY3ldnfRWmfHGiaiwoVNbhwNVbU",
-    authDomain: "salondebelleza3cm50-94de6.firebaseapp.com",
-    projectId: "salondebelleza3cm50-94de6",
-    storageBucket: "salondebelleza3cm50-94de6.appspot.com",
-    messagingSenderId: "429201776728",
-    appId: "1:429201776728:web:ec9ed2f0e5d1ae9c6fea2d",
-    measurementId: "G-3ENJ1BLLWP"
-});
